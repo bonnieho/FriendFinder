79327beba2912f6e8aff7051ed68f8c02ddd364d
bonnieho <bonnieho@rice.edu>
2017-10-01 23:44:16 -0500

    playing with styles

--- app/data/friends.js
+++ app/data/friends.js
@@ -24,7 +24,87 @@ You should save your application's data inside of app/data/friends.js as an arra
 
 module.exports = 
 [
-	{
+{
+  "name":"Dexter Morgan",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Dexter_Morgan.png",
+  "scores":[
+    5, 4, 5, 0, 5, 1, 0, 0, 5, 3
+    ],
+  "bio":"[Seasons 1-8 (all)]<br />  organized"
+},
+{
+  "name":"Deb Morgan",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Deb_Morgan.png",
+  "scores":[
+    0, 5, 0, 0, 2, 2, 0, 0, 0, 0
+    ],
+  "bio":"[Seasons 1-8 (all)]<br />   loyal"
+},
+{
+  "name":"Rudy Cooper (Brian Moser)",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Rudy Cooper.png",
+  "scores":[
+    4, 0, 5, 4, 4, 3, 1, 0, 4, 2
+    ],
+  "bio":"[Season 1]<br />You're obviously detail-oriented and likely drawn to all things science-related for 'Biney' to draw your match. Dexter's long-lost biological brother can be a bro to you, too, and you should solidify that bond over a nice, frozen drink, since's he's gained notoriety as Miami's one and only 'Ice Truck Killer'. Be sympathetic with Brian as he carries on about his romance with Deb and his elaborate scheme to re-connect with Dexter. If not, you could find yourself without a leg to stand on in the end."
+},
+{
+  "name":"Lila Tournay",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Lila_Tournay.png",
+  "scores":[
+    1, 2, 0, 5, 4, 3, 1, 0, 4, 4
+    ],
+  "bio":"[Season 2]<br />An artistic streak must be what brings you to pair with this obsessive sociopathic painter with an insatiable appetite for setting things on fire. If the local bohemian coffeehouse is not in the stars for a meetup, you might instead consider breaking (or burning) bread at the neighborhood Benihana. A word to the wise - don't let this seductress get too attached as she will destroy anything that stands in the way of satisfying her burning desire."
+},
+{
+  "name":"ADA Miguel Prado",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Miguel_Prado.png",
+  "scores":[
+    3, 1, 0, 0, 5, 4, 2, 0, 0, 4
+    ],
+  "bio":"[Season 3]<br />  risk-taker"
+},
+{
+  "name":"Arthur Mitchell",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Arthur_Mitchell.png",
+  "scores":[
+    4, 0, 0, 1, 4, 5, 3, 4, 3, 4
+    ],
+  "bio":"[Season 4]\r\nThere's bound to be a little tension in the air when you hang out with this chosen companion, but don't take it personally as you both share a tendency to simmer and stew about past slights. With the Trinity Killer as a new friend, commiserating about those injustices could strengthen your bond if you make those dates over a cup of soothing chamomile at a quaint teahouse. Unless you want your new-found relationship to drain you of every drop of your being, though, you might want to especially avoid bathtubs around this bloke."
+},
+{
+  "name":"Jordan Chase",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Jordan_Chase.png",
+  "scores":[
+    3, 0, 0, 0, 0, 4, 5, 0, 2, 2
+    ],
+  "bio":"[Season 5]<br />Your new pal is outgoing like you by virtue of being a renowned author and motivational speaker. Although he might deny it over a cappuccino or a hoppy IPA, he's also the leader of the Barrel Girl Gang; a group of his less-savory friends who are a rogue's gallery of sadists responsible for abducting and commiting unspeakable acts of violence to several women before ultimately killing them and stuffing their remains into large barrels."
+},
+{
+  "name":"Travis Marshall",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Travis_Marshall.png",
+  "scores":[
+    0, 2, 0, 1, 1, 1, 0, 5, 2, 1
+    ],
+  "bio":"[Season 6]<br />Something about your spiritual nature pairs you with the Doomsday Killer. Maybe it's biblical prowess or a keen interest in ancient ritual that brings you two together, but whatever the reason, you can surely sacrifice a dark and stormy evening or two scripting plans to hang out over a nice chalice of wine somewhere."
+},
+{
+  "name":"Hannah McKay",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Hannah_McKay.png",
+  "scores":[
+    2, 4, 4, 1, 4, 2, 0, 0, 5, 4
+    ],
+  "bio":"[Season 7]<br />    patient"
+},
+{
+  "name":"Oliver Saxon (Daniel Vogel)",
+  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Oliver_Saxon.png",
+  "scores":[
+    4, 0, 0, 4, 4, 0, 0, 0, 4, 5
+    ],
+  "bio":"[Season 8]<br />An urge to get even is the thread you have in common with your new Brain Surgeon of a companion. If you have half a mind to meet up by the pool for a few brewskis, let him carry on about all of his mommy issues. Anyone with a few brain cells will be able to tell right away that this is the way to win this potential buddy's loyalty."
+}
+	/*{
 	  "name":"Dexter Morgan",
 	  "photo":"http://youkidsgetoffmylawn.org/dexter_images/Dexter_Morgan.png",
 	  "scores":[
@@ -93,5 +173,5 @@ module.exports =
 	  "scores":[
 	    4, 0, 0, 4, 4, 0, 0, 0, 4, 5
 	    ]
-	}
+	}*/
 ]
