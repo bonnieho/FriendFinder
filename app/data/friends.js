ee380f99a4799985a0902e4d8d02a0dd508108d8
bonnieho <bonnieho@rice.edu>
2019-01-27 22:20:28 -0600

    updated with drafts of some of the friend bios

--- app/data/friends.js
+++ app/data/friends.js
@@ -62,7 +62,7 @@ module.exports =
   "scores":[
     3, 1, 0, 0, 5, 4, 2, 0, 0, 4
     ],
-  "bio":"[Season 3]<br />  risk-taker"
+  "bio":"[Season 3]<br />There's something about risking it all that matched you with an amigo such as this. Your assistant district attorney chum is guaranteed to provide countless hours of intelligent conversation over cafecitos or cortados, and once under your spell, he's bound to insist on accompanying you on your next adrenaline-pumping adventure. Keep an eye on your new barrister bud, however, lest he finds you in contempt and that underlying manipulative streak of his gets focused against you when you least expect it."
 },
 {
   "name":"Arthur Mitchell",
@@ -78,7 +78,7 @@ module.exports =
   "scores":[
     3, 0, 0, 0, 0, 4, 5, 0, 2, 2
     ],
-  "bio":"[Season 5]<br />Your new pal is outgoing like you by virtue of being a renowned author and motivational speaker. Although he might deny it over a cappuccino or a hoppy IPA, he's also the leader of the Barrel Girl Gang; a group of his less-savory friends who are a rogue's gallery of sadists responsible for abducting and commiting unspeakable acts of violence to several women before ultimately killing them and stuffing their remains into large barrels."
+  "bio":"[Season 5]<br />Your new pal is outgoing like you by virtue of being a renowned author and motivational speaker. Although he might deny it over a robust Sumatra or a hoppy IPA, he's also the leader of the Barrel Girl Gang; a group of his less-savory friends who are a rogue's gallery of sadists responsible for abducting and commiting unspeakable acts of violence to several women before ultimately killing them and stuffing their remains into large barrels. Despite this comrade's charm, it's best to make other plans if he invites you on a tour of his childhood summer camp, though."
 },
 {
   "name":"Travis Marshall",
@@ -86,7 +86,7 @@ module.exports =
   "scores":[
     0, 2, 0, 1, 1, 1, 0, 5, 2, 1
     ],
-  "bio":"[Season 6]<br />Something about your spiritual nature pairs you with the Doomsday Killer. Maybe it's biblical prowess or a keen interest in ancient ritual that brings you two together, but whatever the reason, you can surely sacrifice a dark and stormy evening or two scripting plans to hang out over a nice chalice of wine somewhere."
+  "bio":"[Season 6]<br />Something about your spiritual nature pairs you with the Doomsday Killer. Maybe it's biblical prowess or a keen interest in ancient ritual that brings you two together, but whatever that revelation is in the end, rest assured both of you can surely sacrifice a dark and stormy evening or two scripting plans to hang out in communion over a nice chalice of sacremental wine somewhere."
 },
 {
   "name":"Hannah McKay",
@@ -102,7 +102,7 @@ module.exports =
   "scores":[
     4, 0, 0, 4, 4, 0, 0, 0, 4, 5
     ],
-  "bio":"[Season 8]<br />An urge to get even is the thread you have in common with your new Brain Surgeon of a companion. If you have half a mind to meet up by the pool for a few brewskis, let him carry on about all of his mommy issues. Anyone with a few brain cells will be able to tell right away that this is the way to win this potential buddy's loyalty."
+  "bio":"[Season 8]<br />An urge to get even is the thread you have in common with your new Brain Surgeon of a companion. If you have half a mind to meet up by the pool for a few brewskis, you will be wise to let him carry on about all of his mommy issues. Anyone with any gray matter left upstairs will be able to tell right away that this is the way to win this potential buddy's loyalty."
 }
 	/*{
 	  "name":"Dexter Morgan",
