903636c82e2d29b66e5b4eb9b9d24373286ce92c
bonnieho <bonnieho@rice.edu>
2019-03-17 23:52:10 -0500

    still working on form label fix for accessibility

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -44,51 +44,51 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know who their match is.
     console.log(req.body);
-/*
-    Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-        With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-            Example:
-                User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-                User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-                Total Difference: 2 + 1 + 2 = 5
-        Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-        The closest match will be the user with the least amount of difference.
-*/
-	var userScores = [];
-	for (i=0; i<req.body.scores.length; i++){
-		userScores.push(parseInt(req.body.scores[i])); // since array members came in a string, I gotta convert!
-	}
+    /*
+        Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+            With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+                Example:
+                    User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+                    User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+                    Total Difference: 2 + 1 + 2 = 5
+            Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+            The closest match will be the user with the least amount of difference.
+    */
+  	var userScores = [];
+  	for (i=0; i<req.body.scores.length; i++){
+  		userScores.push(parseInt(req.body.scores[i])); // since array members came in a string, I gotta convert!
+  	}
 
-	var bff;
-	var smallestDiff = 9999; // ridiculously large number to use for comparison
-	for (i=0; i<friendData.length; i++){
-		var friendScores = friendData[i].scores;
-		var totalDifference = 0;
-		for (j=0; j<userScores.length; j++) {
-			totalDifference += Math.abs(userScores[j]-friendScores[j]); // setting absolute value
-		}
-		if (totalDifference<smallestDiff){ // keep comparing numbers based on smallest until you have run through all of the members in the friendData array
-			bff = friendData[i];
-			smallestDiff = totalDifference;
-		}
-	}
+  	var bff;
+  	var smallestDiff = 9999; // ridiculously large number to use for comparison
+  	for (i=0; i<friendData.length; i++){
+  		var friendScores = friendData[i].scores;
+  		var totalDifference = 0;
+  		for (j=0; j<userScores.length; j++) {
+  			totalDifference += Math.abs(userScores[j]-friendScores[j]); // setting absolute value
+  		}
+  		if (totalDifference<smallestDiff){ // keep comparing numbers based on smallest until you have run through all of the members in the friendData array
+  			bff = friendData[i];
+  			smallestDiff = totalDifference;
+  		}
+  	}
 
-	res.json(bff);
- 
-  });
+  	res.json(bff);
+   
+    });
 
-  // from an earlier assignment - maybe save for a rainy day
-  // ---------------------------------------------------------------------------
-  // clears out the friendData array while working with the functionality.
-  // Don"t worry about it!
+    // from an earlier assignment - maybe save for a rainy day
+    // ---------------------------------------------------------------------------
+    // clears out the friendData array while working with the functionality.
+    // Don't worry about it!
 
-  // app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    // If using .json files and objects
-    // friendData = [];
+    // app.post("/api/clear", function() {
+      // Empty out the arrays of data
+      // If using .json files and objects
+      // friendData = [];
 
 
-  // }); // close app.post
+    // }); // close app.post
 
 }; // function(app)
 
