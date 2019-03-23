fd2cba0ead07d145721a99367bc2a508e6ecf4e8
bonnieho <bonnieho@rice.edu>
2019-03-22 23:17:07 -0500

    still working on a more accurate matching algorithm with least squares

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,7 +20,7 @@ module.exports =
   "name":"Rudy Cooper (Brian Moser)",
   "photo":"http://youkidsgetoffmylawn.org/dexter_images/Rudy_Cooper.png",
   "scores":[
-    4, 0, 5, 4, 4, 3, 1, 0, 4, 2
+    3, 0, 5, 4, 4, 3, 1, 0, 4, 1
     ],
   "bio":"[Season 1]<br />You're obviously detail-oriented and likely drawn to all things science-related for 'Biney' to draw your match. Dexter's long-lost biological brother can be a bro to you, too, and you should solidify that bond over a nice batido or other frozen drink, since's he's gained notoriety as Miami's one and only 'Ice Truck Killer'. Be sympathetic with Brian as he carries on about his romance with Deb and his elaborate scheme to re-connect with Dexter. If not, you could find yourself without a leg to stand on in the end."
 },
@@ -44,7 +44,7 @@ module.exports =
   "name":"Arthur Mitchell",
   "photo":"http://youkidsgetoffmylawn.org/dexter_images/Arthur_Mitchell.png",
   "scores":[
-    4, 0, 0, 1, 4, 5, 3, 4, 3, 4
+    4, 0, 0, 1, 2, 5, 2, 4, 3, 4
     ],
   "bio":"<p>[Season 4]<br />There's bound to be a little tension in the air when you hang out with this chosen companion, but don't take it personally as you both share a tendency to simmer and stew about past slights. With the Trinity Killer as a new friend, commiserating about those injustices could strengthen your bond if you make those dates over a cup of soothing chamomile at a quaint teahouse. Unless you want your new-found relationship to drain you of every drop of your being, though, you might want to especially avoid bathtubs around this bloke.</p>"
 },
@@ -60,7 +60,7 @@ module.exports =
   "name":"Travis Marshall",
   "photo":"http://youkidsgetoffmylawn.org/dexter_images/Travis_Marshall.png",
   "scores":[
-    0, 2, 0, 1, 1, 1, 0, 5, 2, 1
+    0, 2, 2, 1, 1, 1, 0, 5, 2, 1
     ],
   "bio":"[Season 6]<br />Something about your spiritual nature pairs you with the Doomsday Killer. Maybe it's biblical prowess or a keen interest in ancient ritual that brings you two together, but whatever that revelation is in the end, rest assured both of you can surely sacrifice a dark and stormy evening or two scripting plans to hang out in communion over a nice chalice of sacramental wine somewhere."
 },
@@ -68,7 +68,7 @@ module.exports =
   "name":"Hannah McKay",
   "photo":"http://youkidsgetoffmylawn.org/dexter_images/Hannah_McKay.png",
   "scores":[
-    2, 4, 4, 1, 4, 2, 0, 0, 5, 4
+    1, 4, 4, 1, 4, 2, 0, 0, 5, 4
     ],
   "bio":"[Season 7]<br />You will find yourself intrigued with your intoxicatingly toxic florist BFF; wanting to spend lots of quality time together possibly reminiscing about youthful indiscretions. Both of you are exceedingly patient and cunning and can look forward to swapping tales of weaving your own webs to get what you want. Steer clear of libations even as seemingly benign as a lovely cup of herbal tea though, as this new pal with a green thumb and a checkered past has a bad habit of poisoning those who fall out of favor with her."
 },
@@ -76,7 +76,7 @@ module.exports =
   "name":"Oliver Saxon (Daniel Vogel)",
   "photo":"http://youkidsgetoffmylawn.org/dexter_images/Oliver_Saxon.png",
   "scores":[
-    4, 0, 0, 4, 4, 0, 0, 0, 4, 5
+    4, 0, 3, 2, 4, 4, 0, 0, 4, 5
     ],
   "bio":"[Season 8]<br />An urge to get even is the thread you have in common with your new 'Brain Surgeon' of a companion. If you have half a mind to meet up by the pool for a few brewskis (and maybe listen to some Mama Cass), you will be wise to let him carry on about all of his mommy issues. Anyone with any gray matter left upstairs will be able to tell right away that this is the way to win this potential buddy's loyalty."
 }
